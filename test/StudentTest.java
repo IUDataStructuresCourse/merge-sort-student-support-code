441a96a9c5d248353b0d14718ee4dcada2004128
Jeremy G. Siek <jeremy.siek@gmail.com>
2025-02-15 14:04:59 -0500

    update class name

--- test/StudentTest.java
+++ test/StudentTest.java
@@ -5,7 +5,7 @@ import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class MergeSortTest {
+public class StudentTest {
 
     @Test
     public void smallMergeSortArray() {
